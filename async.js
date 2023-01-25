3dc8e035997ec1b08b02e033da0024866e862814
unohayato <mamidori1p1p@gmail.com>
2023-01-25 14:12:04 +0900

    非同期処理2

--- async.js
+++ async.js
@@ -5,3 +5,14 @@ fetch("https://jsonplaceholder.typicode.todos/1")
     console.log(error);
   });
 
+async function main() {
+  try{
+    const res = await fetch("https://jsonplaceholder.typicode.todos/1");
+    const json = await res.json();
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
