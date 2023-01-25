14b7175de6527aaaf50df9d1940bda13552d1283
unohayato <mamidori1p1p@gmail.com>
2023-01-25 13:10:36 +0900

    関数

---
+++ functions.js
@@ -0,0 +1,13 @@
+function square(num) {
+  return num * num;
+}
+
+const square = function(num) {
+  return num * num;
+}
+
+const square = (num) => {
+  return num * num;
+}
+
+const square = num  => num * num
